56fc2658b70357ccc80581aefc73b19e6362ebf1
takahiro fujita <ftakahiro190307@gmaill.com>
2021-10-29 23:19:17 +0900

    example - change example language jp -> en

--- src/index.js
+++ src/index.js
@@ -6,40 +6,22 @@ ReactDOM.render(
 
     <SlimMultipleSelect
         options={[
-            { id: 1, name: "サンプルネーム1" },
-            { id: 2, name: "サンプルネーム2" },
-            {
-                id: 3,
-                name: "sample3ネーム長い文字列のテストです。",
-            },
-            {
-                id: 4,
-                name: "sample3ネーム長い文字列のテストです。さらに長いテストです。４",
-            },
-            {
-                id: 5,
-                name: "sample3ネーム長い文字列のテストです。さらに長いテストです。４",
-            },
-            {
-                id: 6,
-                name: "sample3ネーム長い文字列のテストです。さらに長いテストです。４",
-            },
-            {
-                id: 7,
-                name: "sample3ネーム長い文字列のテストです。さらに長いテストです。４",
-            },
+            { id: 1, name: "sample item 1" },
+            { id: 2, name: "sample item 2" },
+            { id: 3, name: "sample item 3" },
+            { id: 4, name: "sample item 4" },
+            { id: 5, name: "sample item 5" },
+            { id: 6, name: "sample item 6" },
+            { id: 7, name: "sample item 7" },
         ]}
         value={[
-            { id: 1, name: "サンプルネーム1" },
-            { id: 2, name: "サンプルネーム2" },
-            {
-                id: 3,
-                name: "sample3ネーム長い文字列のテストです。",
-            },
+            { id: 1, name: "sample item 1" },
+            { id: 2, name: "sample item 2" },
         ]}
         optLabel='name'
         optKey='id'
-        placeholder='ユーザーを選択してください'
+        placeholder='search item'
+        onHandleChange={(value) => console.log(value)}
     />
  ,
   document.getElementById('root')
